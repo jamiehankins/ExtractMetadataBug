6ac107f57f029453030613b6ec4fe15150ff5223
Jamie A Hankins <jamie.hankins@yubico.com>
2021-07-02 12:08:16 -0700

    Project Builds

---
+++ MainProject/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MainProject
+{
+    /// <summary>
+    /// This is a test class to have something for DocFX to document.
+    /// </summary>
+    public class TestClass
+    {
+        /// <summary>
+        /// This is a constructor to document.
+        /// </summary>
+        public TestClass() { }
+
+        public string DoDad { get; }
+
+        /// <summary>
+        /// Simple method to generate docs for.
+        /// </summary>
+        /// <remarks>
+        /// I'd like to take a moment to thank all of those who helped me get to
+        /// a place where I can write documentation like this.
+        /// </remarks>
+        /// <param name="source">A meaningless boolean value, much like most questions in the world.</param>
+        /// <returns>An exactly equivalently meaningless boolean value, much like most answers in the world.</returns>
+        public bool BoolReturningMethod(bool source) { SupportLibrary.SupportClass.DoNothing(source); return source; }
+    }
+}
---
+++ SupportLibrary/SupportClass.cs
@@ -0,0 +1,19 @@
+﻿namespace SupportLibrary
+{
+    /// <summary>
+    /// A class unto itself.
+    /// </summary>
+    public class SupportClass
+    {
+        /// <summary>
+        /// Constructing the future, one bit at a time.
+        /// </summary>
+        public SupportClass() { }
+
+        /// <summary>
+        /// This method makes a promise and doesn't dissappoint.
+        /// </summary>
+        /// <param name="meaningless">Truth in naming.</param>
+        public static void DoNothing(bool meaningless) { }
+    }
+}
